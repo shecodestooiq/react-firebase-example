504f7998981c48b92f4f64e8e37f4df07fd3fc5c
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2023-09-16 14:38:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,89 +1,9 @@
-import { useState, useEffect } from 'react';
-import { db } from './firebaseConfig';
-import {
-  collection,
-  doc,
-  query,
-  onSnapshot,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-} from 'firebase/firestore';
+import SignUp from './pages/SignUp';
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [nameField, setNameField] = useState('');
-  const [timeField, setTimeField] = useState('');
-
-  useEffect(() => {
-    const q = query(collection(db, 'todos'));
-    onSnapshot(q, (data) => {
-      const finalData = data.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-      console.log(finalData);
-      setTodos(finalData);
-    });
-  }, []);
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    try {
-      await addDoc(collection(db, 'todos'), {
-        name: nameField,
-        time: timeField,
-      });
-    } catch (err) {
-      alert(err);
-    }
-  }
-
-  async function deleteTodo(id) {
-    console.log(id);
-    try {
-      await deleteDoc(doc(db, 'todos', id));
-    } catch (err) {
-      alert(err);
-    }
-  }
-
-  async function updateTodo(id) {
-    console.log(id);
-    try {
-      await updateDoc(doc(db, 'todos', id), {
-        name: nameField,
-        time: timeField,
-      });
-    } catch (err) {
-      alert(err);
-    }
-  }
-
   return (
     <div>
-      <h1>Todos App</h1>
-      <form onSubmit={handleSubmit}>
-        <label>name</label>
-        <input
-          type='text'
-          name='nameField'
-          value={nameField}
-          onChange={(e) => setNameField(e.target.value)}
-        />
-        <label>Time</label>
-        <input
-          type='text'
-          name='nameField'
-          value={timeField}
-          onChange={(e) => setTimeField(e.target.value)}
-        />
-        <button type='submit'>Submit</button>
-      </form>
-      {todos.map((todo) => (
-        <div>
-          {todo.name}{' '}
-          <button onClick={() => updateTodo(todo.id)}>update</button>
-          <button onClick={() => deleteTodo(todo.id)}>delete</button>
-        </div>
-      ))}
+      <SignUp />
     </div>
   );
 }
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SignUp() {
+  return (
+    <div>
+      <h1>Sign up</h1>
+      <div></div>
+    </div>
+  );
+}
+
+export default SignUp;
